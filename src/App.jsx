6d1b7ed9c57658d782b20c86cc7ddff090174639
Savin Osuka <44639335+savin2001@users.noreply.github.com>
2025-01-14 22:49:25 +0300

    Initial Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
