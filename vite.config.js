3563f948b6afb9594a148023dc3371b846d02533
Savin Osuka <44639335+savin2001@users.noreply.github.com>
2025-01-31 23:00:20 +0300

    adding the env variable

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  // Load environment variables
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      host: env.VITE_APP_HOST || 'localhost',
+      port: Number(env.VITE_APP_PORT) || 5173
+    }
+  };
+});
